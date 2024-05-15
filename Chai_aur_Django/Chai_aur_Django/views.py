b5f20a220160da922f0c835a4d0480de0a45d047
Shvam Singh <singhshivam583@yahoo.in>
2024-05-15 22:34:48 +0530

    added template

---
+++ Chai_aur_Django/Chai_aur_Django/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, world. You're at the Django Home")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("You're at the about ")
+
+def contact(request):
+    return HttpResponse("You're at the contact")
+
+
