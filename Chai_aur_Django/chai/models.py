0cc7b8b7ea6bce8d707178c5fad29d25bb57599f
Shvam Singh <singhshivam583@yahoo.in>
2024-05-28 17:41:06 +0530

    added model

--- Chai_aur_Django/chai/models.py
+++ Chai_aur_Django/chai/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    chai_types_enums = [
+        ('BT', 'Black Tea'),
+        ('MT', 'Masala Tea'),
+        ('ET', 'Elaichi Tea'),
+        ('PT', 'Plain Tea'),
+        ('GT', 'Ginger Tea'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chaiImage/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=chai_types_enums)
+    description = models.TextField(default='')
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- Chai_aur_Django/chai/views.py
+++ Chai_aur_Django/chai/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ChaiVarity
 
 # Create your views here.
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais':chais})
+
+def chai_detail(request, chai_id):
+    print(f'Chai Id: {chai_id}')
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'chai/chai_details.html', {'chai':chai})
