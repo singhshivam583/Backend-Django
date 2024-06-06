0fef7a0974c40e7fedcd5a714b4b6bd50fca1b17
Shvam Singh <singhshivam583@yahoo.in>
2024-06-06 17:32:46 +0530

    admin-pannel

--- Chai_aur_Django/chai/admin.py
+++ Chai_aur_Django/chai/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import ChaiVarity
-# Register your models here.
-admin.site.register(ChaiVarity)
+from .models import ChaiVarity, ChaiReview, ChaiCertificate, Store
+
+# Register your models here
+
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReview
+    extra = 2
+
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varity',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+
+admin.site.register(ChaiVarity, ChaiVarityAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
 
